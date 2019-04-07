3df4f8484d2628ea86df49550a1c1ca336d4d82f
hl1653937363 <45041997+hl1653937363@users.noreply.github.com>
2019-04-07 11:27:31 +0800

    Add files via upload

---
+++ xunhuan.js
@@ -0,0 +1,16 @@
+var sArr = ["long"," time"," no"," see"];
+    console.log(sArr.join(""));
+    
+    var str1 = "hello"
+	var str2 = "world"
+	var str3 = "."
+	var str = str1.concat(str2,str3);
+    console.log(str);
+    
+    str = str1+str2+str3;
+    console.log(str);
+    
+    str = "";
+	str += "hello";
+	str += "world";
+	console.log(str);
